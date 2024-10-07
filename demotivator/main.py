04d51d323a3e10f614ae6a24aa997c856cbbd444
сендечка <fanikfil@gmail.com>
2024-10-08 00:19:59 +0800

    Add files via upload

---
+++ demotivator/main.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher, executor, types
+import photo_editor
+from colorama import init, Fore, Style
+
+TOKEN = '7241242475:AAHcHMa1fxWGwsIpnmhdp57GgaxFPCV63i8'
+
+init()
+
+try:
+    bot = Bot(token= TOKEN)
+    dp = Dispatcher(bot)
+
+    @dp.message_handler(commands=['start'])
+    async def start(message: types.Message):
+        await bot.send_message(message.from_user.id, 'приветик\nОтправь мне фото с подписью\n\ndeveloper @x0x1dead, исходный код можете просить у него')
+
+    @dp.message_handler(content_types=['photo'])
+    async def photo(message):
+
+        await message.photo[-1].download('pic/mem.jpg')      
+
+        if message.caption:
+            text = message.caption
+
+            photo_editor.add_text(text)    
+            await bot.send_photo(message.from_user.id, photo=open('pic/paste/ok.jpg', 'rb'))
+        else:
+            await bot.send_message(message.from_user.id, 'Ты забыл добавить подпись')
+
+    if __name__ == '__main__':
+        print('Бот успешно запущен')
+        executor.start_polling(dp, skip_updates=True)
+except:
+    print(Fore.RED + 'Вы забыли ввести токен\nЭто можно сделать в файле main.py' + Fore.RESET)
